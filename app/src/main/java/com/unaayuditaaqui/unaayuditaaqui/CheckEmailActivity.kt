d6e8c26c923a1a61bfd1b5f7321cf1129f19a9da
Luis Loreto <a216207287@unison.mx>
2022-09-24 22:39:06 -0700

    Check Email

---
+++ app/src/main/java/com/unaayuditaaqui/unaayuditaaqui/CheckEmailActivity.kt
@@ -0,0 +1,83 @@
+package com.unaayuditaaqui.unaayuditaaqui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import com.unaayuditaaqui.unaayuditaaqui.databinding.ActivityCheckEmailBinding
+
+class CheckEmailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheckEmailBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_email)
+
+        binding = ActivityCheckEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        val user = auth.currentUser
+
+        binding.veficateEmailAppCompatButton.setOnClickListener {
+            val profileUpdates = userProfileChangeRequest {
+            }
+            user!!.updateProfile(profileUpdates)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        if (user.isEmailVerified) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            this.startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "Por favor verifica tu correo.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+        }
+
+        binding.signOutImageView.setOnClickListener {
+            signOut()
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            if(currentUser.isEmailVerified){
+                reload()
+            } else {
+                sendEmailVerification()
+            }
+        }
+    }
+
+    private fun sendEmailVerification() {
+        val user = auth.currentUser
+        user!!.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Se ha enviado un correo de verifiación.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload() {
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+
+    private  fun signOut(){
+        Firebase.auth.signOut()
+        val intent = Intent(this, SignInActivity::class.java)
+        this.startActivity(intent)
+    }
+}
