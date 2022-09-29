2521f6d5081ead7beeb632ed860093767868b10c
Luis Loreto <a216207287@unison.mx>
2022-09-28 22:29:02 -0700

    Home

--- app/src/main/java/com/unaayuditaaqui/unaayuditaaqui/MainActivity.kt
+++ app/src/main/java/com/unaayuditaaqui/unaayuditaaqui/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.unaayuditaaqui.unaayuditaaqui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.unaayuditaaqui.unaayuditaaqui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private val fileResult = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.signOutImageView.setOnClickListener {
+            signOut()
+        }
+
     }
+
+    private  fun signOut(){
+        auth.signOut()
+        val intent = Intent(this, SignInActivity::class.java)
+        this.startActivity(intent)
+    }
+
+
 }
