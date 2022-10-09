7aa88efc85cae210f217dd84b1bff6d8d4b0a05c
Luis Loreto <a216207287@unison.mx>
2022-10-08 20:43:17 -0700

    Activity Main V3

--- app/src/main/java/com/unaayuditaaqui/unaayuditaaqui/MainActivity.kt
+++ app/src/main/java/com/unaayuditaaqui/unaayuditaaqui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.unaayuditaaqui.unaayuditaaqui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.unaayuditaaqui.unaayuditaaqui.databinding.ActivityMainBinding
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +24,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
+        database = FirebaseDatabase.getInstance()
+
+        updateUI ()
+
+        binding.navHome.setOnClickListener{
+            Toast.makeText(this, "Home.",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        binding.navAccount.setOnClickListener{
+            val intent = Intent(this, MyAccountActivity::class.java)
+            this.startActivity(intent)
+        }
 
         binding.signOutImageView.setOnClickListener {
             signOut()
         }
+
+
 
     }
 
@@ -32,4 +51,30 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SignInActivity::class.java)
         this.startActivity(intent)
     }
+
+    private  fun updateUI () {
+        val user = auth.currentUser
+
+        if (user != null){
+            binding.emailTextView.text = user.email
+
+            if(user.displayName != null){
+                binding.nameTextView.text = user.displayName
+            }
+
+            Glide
+                .with(this)
+                .load(user.photoUrl)
+                .centerCrop()
+                .placeholder(R.drawable.ic_profile)
+                .into(binding.profileImageView)
+            Glide
+                .with(this)
+                .load(user.photoUrl)
+                .centerCrop()
+                .placeholder(R.drawable.ic_profile)
+                .into(binding.bgProfileImageView)
+        }
+    }
+
 }
